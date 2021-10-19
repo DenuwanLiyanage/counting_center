7a51e35cc27e13fec96af2d56f317e1c33eaeb54
Sachintha Denuwan Liyanage <48705649+DenuwanLiyanage@users.noreply.github.com>
2021-10-19 09:37:13 +0530

    Initial commit

---
+++ src/main/java/com/counting_center/counting_center/CountingCenterApplication.java
@@ -0,0 +1,13 @@
+package com.counting_center.counting_center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountingCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountingCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/counting_center/counting_center/CountingCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.counting_center.counting_center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountingCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
